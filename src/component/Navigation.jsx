300b76b197ac190da3d70b51f83d084ffdf2ffd2
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-15 12:03:08 +0100

    making some necessary adjustment

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,19 +1,18 @@
 import './Style.css';
-import React, { useState } from 'react';
-import {MdOutlineNightlight, MdOutlineLightMode} from "react-icons/md"
-
+import React from 'react';
+import {MdOutlineNightlight} from "react-icons/md"
+// , MdOutlineLightMode
 
 
 function Navigation({ handle }) {
 
-  const [view, setview] = useState(false);
+  // const [view, setview] = useState(false);
 
   return (
     <nav>
       <h2>Where in the World?</h2>
-      {
-          view ? <button onClick={handle}><MdOutlineLightMode className='icon'/>Light Mode</button> : <button onClick={handle}><MdOutlineNightlight className='icon'/> Dark Mode</button>
-      }
+      <button onClick={handle}><MdOutlineNightlight className='icon'/> Dark Mode</button>
+                {/* view ? <button onClick={handle}><MdOutlineLightMode className='icon'/>Light Mode</button> : */}
       
     </nav>
   );
